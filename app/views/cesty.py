d6b83f8935c80e77a563e9251150cc9da26363a2
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2024-04-29 20:44:50 +0200

    Create cesty.py

---
+++ app/views/cesty.py
@@ -0,0 +1,31 @@
+from app import app, request, render_template, redirect, session, url_for, make_response
+import os
+import json
+
+from flask import jsonify
+
+
+
+@app.route("/")
+def zakladni():
+    return render_template("web.html")
+
+
+
+
+
+
+
+@app.route("/cesta")
+def cesta():
+    objekt = {
+        "klic1": "ahoj",
+        "klic2": 123,
+        "klic3": 3.1415,
+        "pole": [1, 2, 3, 4],
+        "objekt": {
+            "klic11": "test",
+        }
+    }
+
+    return jsonify(objekt)
