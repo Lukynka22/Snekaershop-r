57c54755b431977c96a748b46569e34918690b74
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2024-06-05 17:18:54 +0200

    Dokonceni funcke pro pridani profiloové dotkY dalsi.js

--- app/static/dalsi.js
+++ app/static/dalsi.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const fileInput = document.getElementById("file-input");
     const profilePic = document.getElementById("profile-pic");
+
+    fileInput.addEventListener("change", function() {
+        const file = fileInput.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function() {
+            profilePic.src = reader.result;
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    });
+});
