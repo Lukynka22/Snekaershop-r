a0fd05e460a2dffffca3d135ab9de62d18022481
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2024-06-05 17:18:10 +0200

    vytvoreni + pridani do JS dalsi.js

---
+++ app/static/dalsi.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fileInput = document.getElementById("file-input");
+    const profilePic = document.getElementById("profile-pic");
