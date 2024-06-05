52460739d08204cbf8e931879891d61a49d7edf8
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2024-06-05 16:22:02 +0200

    Přidání k tomu Kontakty.js

--- app/static/Kontakty.js
+++ app/static/Kontakty.js
@@ -1,3 +1,5 @@
 function openEmailMenu(email) {
     var subject = "Předmět emailu";
     var body = "Tělo emailu";
+
+    var mailtoUrl = "mailto:" + email + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
