308ff37d57acd27cfe106854bc9ec170137907c8
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2024-06-06 15:21:44 +0200

    Pridani podminky pro tmavý rezium dark.js

--- app/static/dark.js
+++ app/static/dark.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleButton = document.getElementById('theme-toggle');
     const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+    }
