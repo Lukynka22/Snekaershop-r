48c90f9f0325b0534c6725d1774e7644ceca2f6b
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2024-06-06 15:21:15 +0200

    Vytvoření funcke pro tmavý rezim dark.js

---
+++ app/static/dark.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
